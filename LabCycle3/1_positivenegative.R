5fe1f54a4cace9ada44c12cd962f50aec0c42452
Aditeya J Frankur <88970191+aditeya24@users.noreply.github.com>
2025-07-11 15:47:43 +0530

    Added 1_positivenegative.R

---
+++ LabCycle3/1_positivenegative.R
@@ -0,0 +1,13 @@
+check_sign <- function(num) {
+  if(num>0) {
+    cat(num, "is Positive\n")
+  } else if(num<0) {
+    cat(num, "is Negative\n")
+  } else {
+    cat("Number is Zero\n")
+  }
+}
+
+
+num <- as.numeric(readline("Enter num: "))
+check_sign(num)
